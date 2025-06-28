bb9d87787b88d96275fa9bf7590208b4f494c558
salmasbai <essbaisalma0@gmail.com>
2025-06-28 20:33:25 +0100

    Mise à jour de la base de données

--- admin/gestion_etudiants.sql
+++ admin/gestion_etudiants.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : sam. 28 juin 2025 à 19:23
+-- Généré le : sam. 28 juin 2025 à 15:50
 -- Version du serveur : 9.1.0
 -- Version de PHP : 8.4.0
 
